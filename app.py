bc5da7f635dbd64227e65ec48ef312b64e850172
SHINJINSU <jsshin8128@gmail.com>
2024-08-18 23:18:36 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from models import db, Todo
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    todos = Todo.query.all()
+    return jsonify([todo.to_dict() for todo in todos])
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    data = request.get_json()
+    new_todo = Todo(title=data['title'])
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify(new_todo.to_dict()), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
